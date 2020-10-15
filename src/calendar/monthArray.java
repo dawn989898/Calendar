870a21bd845ba213bd92f09fa3955d40f7d1a397
dawn <ansgywjd321@gmail.com>
2020-10-15 22:50:01 +0900

    update use class method

--- src/calendar/monthArray.java
+++ src/calendar/monthArray.java
@@ -3,13 +3,20 @@ package calendar;
 import java.util.Scanner;
 
 public class monthArray {
-	public static void main(String... args){
-	int[] maxDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	Scanner scan = new Scanner(System.in);
-	System.out.println("달을 입력하세요.");
-	int month = scan.nextInt();
-	System.out.printf("%d월은 %d일까지 있습니다.",month, maxDays[month-1]);
-	scan.close();
+	private static final int[] maxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int maxDayOfMonth(int month) {
+		return maxDays[month - 1];
+	}
+
+	public static void main(String... args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int month = scan.nextInt();
+		monthArray mon = new monthArray();
+		System.out.printf("%d월은 %d일까지 있습니다.", month, mon.maxDayOfMonth(month));
+		scan.close();
+
 	}
 }

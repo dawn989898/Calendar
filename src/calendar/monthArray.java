56a4e5397990e523745a34e6fd37e9435c519473
dawn <ansgywjd321@gmail.com>
2020-10-15 23:23:23 +0900

    update loop

--- src/calendar/monthArray.java
+++ src/calendar/monthArray.java
@@ -12,11 +12,22 @@ public class monthArray {
 	public static void main(String... args) {
 
 		Scanner scan = new Scanner(System.in);
+		System.out.println("반복횟수를 입력하세요");
+		int loop = scan.nextInt();
+		
 		System.out.println("달을 입력하세요.");
+		
+		int[] months = new int[loop];
+		
+		for(int i=0; i<loop; i++) {
 		int month = scan.nextInt();
+		months[i] += month;
+		}
+		
+		for(int i=0; i<loop; i++) {
 		monthArray mon = new monthArray();
-		System.out.printf("%d월은 %d일까지 있습니다.", month, mon.maxDayOfMonth(month));
+		System.out.printf("%d월은 %d일까지 있습니다. \n", months[i], mon.maxDayOfMonth(months[i]));
 		scan.close();
-
+		}
 	}
 }

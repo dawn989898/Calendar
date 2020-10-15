a886b7b5c9eb8b6709d0b4430b7ccaf3ddcdda29
dawn <ansgywjd321@gmail.com>
2020-10-15 21:51:52 +0900

    correct wrong result month start 0

--- src/calendar/month.java
+++ src/calendar/month.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class month {
 
 	public static void main(String... args){
-		/*
-		 * 달을 입력하세요. 3 (엔터) 3월은 31일까지 있습니다.
-		 */
-    	// create a calendar  
-		Calendar cal = Calendar.getInstance();
+		//java.time api로 고치기
+		//month는 0부터 시작한대
 		Scanner scan = new Scanner(System.in);
 		System.out.println("달을 입력하세요.");
 		int month = scan.nextInt();
-		cal.set(Calendar.MONTH, month);
-
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MONTH, month+1);
+		
 		int day = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 		System.out.printf("%d월은 %d일까지 있습니다.",month, day);
-		scan.close();
+		
+		System.out.println("Changed year is :- " + cal.get(Calendar.YEAR));
+        System.out.println("Changed month is :- " + cal.get(Calendar.MONTH));
+		System.out.println("----------------------------------------");
+		
+		
+		
     }
 }
